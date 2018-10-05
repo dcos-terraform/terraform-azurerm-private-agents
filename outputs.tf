7b53f04dd0a39ef08630d63a33875bb40b89fb9f
Jan Ulferts <julferts@mesosphere.com>
2018-10-05 12:46:45 +0200

    cleanup outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "admin_username" {
   description = "admin username"
-  value       = "${module.dcos-pvtagt-instances.admin_username}"
+  value       = "${module.dcos-private-agents-instances.admin_username}"
 }
 
 output "private_ips" {
   description = "List of private ip addresses created by this module"
-  value       = ["${module.dcos-pvtagt-instances.private_ips}"]
+  value       = ["${module.dcos-private-agents-instances.private_ips}"]
 }
 
 output "public_ips" {
   description = "List of public ip addresses created by this module"
-  value       = ["${module.dcos-pvtagt-instances.public_ips}"]
+  value       = ["${module.dcos-private-agents-instances.public_ips}"]
 }
 
 output "prereq_id" {
   description = "prereq id"
-  value       = "${module.dcos-pvtagt-instances.prereq_id}"
+  value       = "${module.dcos-private-agents-instances.prereq_id}"
 }
